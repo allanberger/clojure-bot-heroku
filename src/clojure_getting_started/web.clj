ba50d72ea23a3fda4d88174117c91a69ff51a58d
Jon Mountjoy <jon@heroku.com>
2016-08-11 11:46:57 +0100

    Add button

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -9,7 +9,7 @@
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :body "Hello from Heroku with Clojure"})
 
 (defroutes app
   (GET "/" []
